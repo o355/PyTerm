a42e4985df1060e1baaadb5d26e0480ba42cde21
o355 <owen@owenthe.ninja>
2016-11-07 10:18:27 -0500

    ping program is done
    +1
    Next comes more unification hurray!

--- pterm.py
+++ pterm.py
@@ -133,29 +133,52 @@ while not done:
         from random import randint
         print("")
         ping_score = 0
+        ping_mistakes = 0
         ping_loop = False
         while not ping_loop:
             print("Do you ping or do you pong?")
-            ping_int = randint(0, 1)
+            ping_int = randint(0, 3)
             ping_input = input("ping_input-userinput@ping-prgm-" + promptver2).lower()
             if (ping_input == "ping" and ping_int == 0):
                 print("Wake me up. Wake me up inside. Can't wake up. Wake me up inside. Save me.")
                 print("Your score:", ping_score)
                 break
-            elif (ping_input == "ping" and ping_int) == 1:
+            elif (ping_input == "ping" and ping_int == 1):
                 print("Hurrah! Let's keep pinging.")
                 ping_score = ping_score + 1
                 continue
-            elif (ping_input == "pong" and ping_int) == 0:
-                print("Hurrah! Let's keep pinging.")
-                ping_score = ping_score +1
-                continue
-            elif (ping_input == "pong" and ping_int) == 1:
+            elif (ping_input == "ping" and ping_int == 2):
                 print("Wake me up. Wake me up inside. Can't wake up. Wake me up inside. Save me.")
                 print("Your score:", ping_score)
                 break
+            elif (ping_input == "ping" and ping_int == 3):
+                print("Hurrah! Let's keep pinging.")
+                ping_score = ping_score + 1
+                continue
+            elif (ping_input == "pong" and ping_int == 0):
+                print("Hurrah! Let's keep ponging.")
+                ping_score = ping_score + 1
+                continue
+            elif (ping_input == "pong" and ping_int == 1):
+                print("Wake me up. Wake me up inside. Can't wake up. Wake me up inside. Save me.")
+                print("Your score:", ping_score)
+                break
+            elif (ping_input == "pong" and ping_int == 2):
+                print("Hurrah! Let's keep ponging.")
+                ping_score = ping_score + 1
+                continue
+            elif (ping_input == "pong" and ping_int == 3):
+                print("Wake me up. Wake me up inside. Can't wake up. Wake me up inside. Save me.")
+                print("Your score:", ping_score)
+                break
+            elif ping_mistakes == 10:
+                print("Stop making so many mistakes.")
+                print("")
+                break
             else:
                 print("Did you read the instructions?")
+                ping_mistakes = ping_mistakes + 1
+                print("")
                 continue
         continue
     elif cmd == "alwayssave":
