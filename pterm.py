1753ef9bd1b9de6716bf73704f6bee9e020a032b
o355 <owen@owenthe.ninja>
2016-12-14 11:20:37 -0500

    Back to PyTerm

--- pterm.py
+++ pterm.py
@@ -72,6 +72,7 @@ if sys.platform.startswith('win32'):
             pyaudioinstall = False
             
             
+            
     else:
         print("")
         print("Now booting PyTerm.")
