fe0371a7058dff866e381352c09142a1bcefba82
o355 <owen@owenthe.ninja>
2016-12-16 12:03:42 -0500

    Another fix for issue #10
    I was running 3.5.2...

--- pterm.py
+++ pterm.py
@@ -35,7 +35,7 @@ except ImportError:
     raise ImportError("You don't have sys! Please install sys to run PyTerm.")
 print(round(time.time() - entireload,4), "| Imported sys!")
 print(round(time.time() - entireload,4), "| Checking Python version...")
-if sys.version_info[0] < 3.5:
+if sys.version_info[0] >= 3.5:
     print("You must be using Python 3.5 or greater to run PyTerm.")
     sys.exit()
 print(round(time.time() - entireload,4), "| Running Python 3.5 or greater!")
